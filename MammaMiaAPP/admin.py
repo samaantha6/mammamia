b0934ede9cb6615120e3cebd9d99787b0d94944e
soferpel <so.fernandez@opendeusto.es>
2024-10-29 13:45:47 +0100

    creacion modelos

--- MammaMiaAPP/admin.py
+++ MammaMiaAPP/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Menu, Plato, Ingrediente
 
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Plato)
+admin.site.register(Ingrediente)
--- MammaMiaAPP/models.py
+++ MammaMiaAPP/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.PositiveIntegerField(default = 0)
+    descripcion = models.CharField(max_length=300)
+
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    orden =  models.IntegerField(default = 0)
+    menu = models.ForeignKey(Menu, related_name='platos', on_delete=models.CASCADE)
+
+
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=50)
+    alergeno = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+    plato = models.ForeignKey(Plato, related_name='ingredientes', on_delete=models.CASCADE)
+
