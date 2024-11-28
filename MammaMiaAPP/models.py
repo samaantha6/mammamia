3ac4874c309492f837bf29fd7eebec6a50b3bd5e
soferpel <so.fernandez@opendeusto.es>
2024-11-28 09:27:25 +0100

    ingredientes?

--- MammaMiaAPP/models.py
+++ MammaMiaAPP/models.py
@@ -7,18 +7,26 @@ class Menu(models.Model):
     precio = models.PositiveIntegerField(default = 0)
     descripcion = models.CharField(max_length=300)
 
+#    def __str__(self):
+#        return f"{self.nombre}"
+
 
 class Plato(models.Model):
     nombre = models.CharField(max_length=50)
     tipo = models.CharField(max_length=50)
     orden =  models.IntegerField(default = 0)
     imagenPlato = models.URLField(max_length=60, null=True, blank=True)
-    menu = models.ForeignKey(Menu, related_name='platos', on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, related_name='platos', on_delete=models.CASCADE, null=True, blank=True)
+
+#    def __str__(self):
+#        return f"{self.nombre}"
 
 
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
     alergeno = models.CharField(max_length=50)
     categoria = models.CharField(max_length=50)
-    plato = models.ForeignKey(Plato, related_name='ingredientes', on_delete=models.CASCADE)
-
+    plato = models.ManyToManyField(Plato, related_name='ingredientes')
+    
+#    def __str__(self):
+#        return f"{self.nombre}" 
